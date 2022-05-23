d2e725c6ac66ce927de99a1f35e666a476f76429
nhandienkha <82799728+khand1@users.noreply.github.com>
2022-05-23 14:54:09 +0700

    enable build

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace TESTJENKINS
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TESTJENKINS", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TEST JENKINS", Version = "v1" });
             });
         }
 
